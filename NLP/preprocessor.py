0a040098f1b0a4f1e42ad69280909adf8988abbd
Ina-Son <33450465+Ina-Son@users.noreply.github.com>
2020-06-15 04:55:09 +0900

    자연어처리 누락된 코드 추가

---
+++ NLP/preprocessor.py
@@ -0,0 +1,16 @@
+def preprocess(text):
+    for idx in range(len(text)):
+        line = text[idx]
+
+        line = line.replace('다음 주', '다음주')
+        line = line.replace('이번 주', '이번주')
+        line = line.replace('째 주', '째주')
+        line = line.replace('이번 달', '이번달')
+        line = line.replace('다음 달', '다음달')
+        line = line.replace('오후 ', '오후')
+        line = line.replace('오전 ', '오전')
+        line = line.replace('시 반', '시반')
+        
+        text[idx] = line
+
+    return text
