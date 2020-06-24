55fb4e7bf33d20f42e9bb9b010ea3de8daef97d0
Ina-Son <33450465+Ina-Son@users.noreply.github.com>
2020-06-24 15:41:51 +0900

    OCR 띄어쓰기 문제 해결
    띄어쓰기 문제 해결

--- object_detection/ocr/OCR.py
+++ object_detection/ocr/OCR.py
@@ -13,8 +13,9 @@ class TextDetection:
     def __init__(self):
         self.client = vision.ImageAnnotatorClient()
 
+    """
     def detect_text(self, path):
-        """Detects text in the file."""
+        #Detects text in the file.
         
         with io.open(path, 'rb') as image_file:
             content = image_file.read()
@@ -43,9 +44,9 @@ class TextDetection:
                     response.error.message))
             
         return bounds, texts
-
+    
     def detect_document(self, path):
-        """Detects document features in an image."""
+        #Detects document features in an image.
 
         # [START vision_python_migration_document_text_detection]
         with io.open(path, 'rb') as image_file:
@@ -77,3 +78,52 @@ class TextDetection:
                     response.error.message))
 
         return sentences
+    """
+
+    def detect_document(self, path):
+        with io.open(path, 'rb') as image_file:
+            content = image_file.read()
+
+        image = vision.types.Image(content=content)
+
+        response = self.client.document_text_detection(image=image)
+        texts = response.text_annotations
+        entire_text = texts[0].description.replace('\n', ' ')    
+        
+        sentences = []
+        word_list = []
+        for page in response.full_text_annotation.pages:
+            for block in page.blocks:
+                for paragraph in block.paragraphs:
+                    for word in paragraph.words:
+                        word_text = ''.join([
+                            symbol.text for symbol in word.symbols
+                        ])
+                        word_list.append(word_text)
+                    sentence = ''.join([w for w in word_list])
+                    sentences.append(tuple((paragraph.bounding_box, sentence)))
+                    word_list = []
+                    
+        idx = 0
+        prev = 0
+        result = []
+        
+        for bound, text in sentences:
+            sz = 0
+            while sz < len(text):
+                if entire_text[idx] != ' ':
+                    sz += 1
+                idx += 1
+            
+            idx += 1     
+            result.append((bound, entire_text[prev:idx].strip()))
+            prev = idx
+
+
+        if response.error.message:
+            raise Exception(
+                '{}\nFor more info on error messages, check: '
+                'https://cloud.google.com/apis/design/errors'.format(
+                    response.error.message))
+
+        return result
